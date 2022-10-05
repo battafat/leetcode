8b3459ff33470b0bbe15430421225de96f4508f4
timothy battafarano <tim@deepsprings.edu>
2022-10-05 14:17:38 -0400

    accepted solution but runtime too long to submit

--- Problems/container_with_most_water.py
+++ Problems/container_with_most_water.py
@@ -1,17 +1,23 @@
 class Solution:
     def maxArea(self, heights: List[int]) -> int:
+        max_area = 0
         for index, bar in enumerate(heights):
             #print("line 4, index", index)
             #print("line 5, bar", bar)
             print("")
             for index_2, bar_2 in enumerate(heights):
-                print("index", index)
-                print("bar", bar)
-                print("index_2", index_2)
-                print("bar_2", bar_2)
 
-                vertical = bar if bar < bar_2 else bar_2
-                horizontal = abs(bar - bar_2)
+               # vertical = bar if bar < bar_2 else bar_2
+                vertical = min(bar,bar_2)
+                horizontal = abs(index - index_2)
+
                 area = vertical * horizontal
-                print("area", area)
-                print("")
+
+                if area > max_area:
+                    max_area = area
+                   
+        return(max_area)
+
+
+        # O(n)
+        # O(n^2)
